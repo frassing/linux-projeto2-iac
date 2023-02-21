593f54ca5f3e7937c68fd1e8e3cc827d15f4349e
Raquel Roessing <116108339+frassing@users.noreply.github.com>
2023-02-20 20:24:31 -0400

    Fix text lines 3 and 10

--- script-iac2.sh
+++ script-iac2.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "Atualizando o sistema..."
+echo "Atualizando o servidor..."
 
 apt-get update
 apt-get upgrade -y
 apt-get install apache2 -y
 apt-get install unzip -y
 
-echo "Baixando e copiando arquivos d aaplicação..."
+echo "Baixando e copiando arquivos da aplicação..."
 
 cd /tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
